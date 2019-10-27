875743f60eb02c06ba8238af27fd865584fabf00
Alan Gomes <alanhr2@gmail.com>
2019-10-26 21:46:15 -0300

    add validator

---
+++ server/src/middleware/requestValidator.js
@@ -0,0 +1,47 @@
+import R from 'ramda'
+
+import isEmpty from '../utils/isEmpty'
+
+const formatError = R.compose(
+  item => ({
+    [R.prop('path', item)]: R.prop('message', item),
+  }),
+  R.evolve({
+    path: R.join('.'),
+  }),
+  R.pick(['message', 'path']),
+)
+
+const buldErros = R.reduce(
+  (acc, elm) =>
+    R.compose(
+      R.mergeRight(acc),
+      formatError,
+    )(elm),
+  {},
+)
+
+export default (schema, field = 'body') => (req, res, next) => {
+  const error = R.compose(
+    R.ifElse(
+      isEmpty,
+      R.identity,
+      R.compose(
+        buldErros,
+        R.prop('details'),
+      ),
+    ),
+    R.propOr(null, 'error'),
+    R.partialRight(R.bind(schema.validate, schema), [
+      {
+        abortEarly: false,
+      },
+    ]),
+  )(R.prop(field, req))
+
+  return isEmpty(error)
+    ? next()
+    : res.status(400).json({
+        error,
+      })
+}
---
+++ server/src/middleware/requestValidator.test.js
@@ -0,0 +1,66 @@
+import joi from '@hapi/joi'
+
+import requestValidator from './requestValidator'
+
+describe('RequestValidator', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  const schema = joi.object({
+    name: joi
+      .string()
+      .empty('')
+      .max(50)
+      .required(),
+    description: joi.string().max(100),
+  })
+  const validatorMiddleware = requestValidator(schema)
+
+  test('should validation the body payload and return error', () => {
+    const nextMock = jest.fn()
+    const resMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+
+    validatorMiddleware(
+      {
+        body: {
+          name: '',
+          description: 433,
+        },
+      },
+      resMock,
+      nextMock,
+    )
+
+    expect(resMock.status).toHaveBeenCalledWith(400)
+    expect(resMock.json).toHaveBeenCalledWith([
+      { error: { description: '"description" must be a string', name: '"name" is required' } },
+    ])
+  })
+
+  test.only('should validation the body payload and call next', () => {
+    const nextMock = jest.fn()
+    const resMock = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+
+    validatorMiddleware(
+      {
+        body: {
+          name: 'Teste',
+          description: 'Teste Oodf',
+        },
+      },
+      resMock,
+      nextMock,
+    )
+
+    expect(resMock.json).not.toHaveBeenCalled()
+
+    expect(nextMock).toHaveBeenCalled()
+  })
+})
